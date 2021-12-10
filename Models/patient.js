c9c4b4db35d190c659e7237955502f180b3e7ca1
JatJax <n01424733@unf.edu>
2021-12-10 13:33:49 -0500

    Update patient

--- Models/patient.js
+++ Models/patient.js
@@ -1,9 +1,9 @@
 const patients = [
-    {userId: 1, type: "Diabetic", year: "2011"},
-    {userId: 2, type: "Hypertensive", year: "2012"},
-    {userId: 3, type: "Misc", year: "2013"},
-    {userId: 4, type: "Hypertensive", year: "2014"},
-    {userId: 5, type: "Diabetic", year: "2015"}
+    {userId: 1, amount: "5", year: "2011"},
+    {userId: 2, amount: "10", year: "2012"},
+    {userId: 3, amount: "3", year: "2013"},
+    {userId: 4, amount: "12", year: "2014"},
+    {userId: 5, amount: "6", year: "2015"}
 ]
 
 exports.getPatients = () => {return patients} ; 
