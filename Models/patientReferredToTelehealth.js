343134f9fd682a4b83cf2f633a73fe23ddf851a7
Trey Cox <tcollincox@gmail.com>
2022-02-09 14:35:56 -0500

    adding models and routes

--- Models/patientReferredToTelehealth.js
+++ Models/patientReferredToTelehealth.js
@@ -1,14 +1,14 @@
 const sequelize = require('sequelize');
 const db = require('../config/database');
 
-const patient = db.define('patientEncounter', {
+const patient = db.define('PatientReferredToTelehealth', {
+	PatientType:{
+		type: sequelize.STRING
+	},
 	PatientAmount:{
 		type: sequelize.INTEGER
 	},
 	RecordedYear:{
-		type: sequelize.INTEGER
-	},
-	TargetNumber:{
 		type: sequelize.INTEGER
 	},
 }, {
--- Models/twentyFivePerTelehealthReferred.js
+++ Models/twentyFivePerTelehealthReferred.js
@@ -1,18 +1,24 @@
 const sequelize = require('sequelize');
 const db = require('../config/database');
 
-const patient = db.define('patientEncounter', {
-	PatientAmount:{
+const patients = db.define('twentyFivePerTelehealthReferred', {
+	TargetPercent:{
+		type: sequelize.INTEGER
+	},
+	PercentAchived:{
 		type: sequelize.INTEGER
 	},
 	RecordedYear:{
 		type: sequelize.INTEGER
 	},
-	TargetNumber:{
+	Numerator:{
+		type: sequelize.INTEGER
+	},
+	Denominator:{
 		type: sequelize.INTEGER
 	},
 }, {
 	freezeTableName: true
 });
 
-module.exports = patient;
+module.exports = patients;
