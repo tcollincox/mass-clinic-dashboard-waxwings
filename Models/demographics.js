2db0419aad7f3cd6de3a86ca836a1f68c12b0a8f
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2022-02-11 22:42:30 -0500

    demographics model fix

--- Models/demographics.js
+++ Models/demographics.js
@@ -16,21 +16,3 @@ const demographics = db.define('PatientDemographics', {
 });
 
 module.exports = demographics;
-const sequelize = require('sequelize');
-const db = require('../config/database');
-
-const demographics = db.define('PatientDemographics', {
-    DemographicType: {
-        type: sequelize.STRING
-    },
-    PatientAmount: {
-        type: sequelize.INTEGER
-    },
-    RecordedYear: {
-        type: sequelize.INTEGER
-    }
-}, {
-    freezeTableName: true
-});
-
-module.exports = demographics;
