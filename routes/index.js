215e76f25d71042e1b87d4873af9d7991d6b1ee0
Trey Cox <tcollincox@gmail.com>
2022-03-02 14:46:52 -0500

    partials in the index

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,43 @@ const Applications = require('../Models/applications');
 let applications = [];
 const BloodA1C = require('../Models/bloodA1C');
 let bloodA1C = [];
+const ReferralsMassClininc = require('../Models/patientReferredMassClinic');
+let referralsMassClinic = [];
+const PatientFollowUp = require('../Models/followups');
+let patientFollowUp = [];
+const NoShowPatient = require('../Models/noShowPatient');
+let noShowPatient = [];
+const  PatientAccessibility= require('../Models/patientAccessibility');
+let patientAccessibility = [];
+const PatientArrivalTime = require('../Models/patientArrivalTime');
+let patientArrivalTime = [];
+const PlaceOfCare = require('../Models/placeOfCare');
+let placeOfCare = [];
+const UniquePatientInYear = require('../Models/uniquePatientInYear');
+let uniquePatientInYear = [];
 
+router.get('/', async function(req, res){
+    applications = await Applications.findAll({attributes: ['ApplicationType', 'PatientAmount', 'RecordedYear']})
+    bloodA1C = await BloodA1C.findAll({attributes: ['TimePeriod', 'NumDom', 'TotalPercent', 'PatientAmountSpecifc', 'PatientAmountTotal' ]})
+    referralsMassClinic = await ReferralsMassClininc.findAll({attributes: ['ToFrom', 'PatientAmount', 'RecordedYear']})
+    patientFollowUp = await PatientFollowUp.findAll({attributes: ['PatientType', 'PatientAmount', 'RecordedYear']})
+    noShowPatient = await NoShowPatient.findAll({attributes: ['RecordedYear', 'PercentAmount' ]});
+    patientAccessibility = await PatientAccessibility.findAll({attributes: ['AmountOfTime', 'PercentAmount', 'TotalAmount', 'SpecifcAmount']});
+    patientArrivalTime = await PatientArrivalTime.findAll({attributes: ['TimeArrived', 'PercentAmount', 'PatientAmount']});
+    placeOfCare = await PlaceOfCare.findAll({attributes: ['AppttypeGroup', 'AppttypeSpecific', 'PatientEncounters', 'UniquePatients' ]});
+    uniquePatientInYear = await UniquePatientInYear.findAll({attributes: ['AmountOfYears', 'PatientAmount']});
 
-router.get('/', (req, res) =>{
-    Applications.findAll({attributes: ['ApplicationType', 'PatientAmount', 'RecordedYear']})
-    .then(application => applications = application)
-    .then(BloodA1C.findAll({attributes: ['TimePeriod', 'NumDom', 'TotalPercent', 'PatientAmountSpecifc', 'PatientAmountTotal' ]}))
-    .then(blood => bloodA1C = blood)
-    .then(res.render('index.ejs', {data :{applications:applications, bloodA1C:bloodA1C}}))
-});
+    res.render('index.ejs', {data : {
+        applications:applications, 
+        bloodA1C:bloodA1C,
+        referralsMassClinic:referralsMassClinic,
+        patientFollowUp,patientFollowUp,
+        noShowPatient:noShowPatient, 
+        patientAccessibility:patientAccessibility , 
+        patientArrivalTime:patientArrivalTime,
+        placeOfCare:placeOfCare, 
+        uniquePatientInYear:uniquePatientInYear
+    }
+    })});
 
 module.exports =  router;
